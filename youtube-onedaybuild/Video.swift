7751c12e4e6570ab7a376676eb4849ba932e9282
Christopher Revells <revellsc@gmail.com>
2020-12-07 15:22:15 -0500

    modified gitignore to stop tracking Constants.swift

---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Christopher Revells on 12/2/20.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
