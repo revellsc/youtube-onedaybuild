295720f9bfc10d87d76d3d5ecb3388d4a5c64da1
Christopher Revells <revellsc@gmail.com>
2020-12-16 11:05:42 -0500

    added ConstantsTemplate, modified gitignore to stop tracking Constants.swif, added ConstantsTemplate.swift instead

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Christopher Revells on 12/16/20.
+//
+
+/*
+ Add your own keys below and uncomment the code
+*/
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+}
+*/
